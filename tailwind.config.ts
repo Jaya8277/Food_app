b7adc446df9df898b8c7ddff7e0249875db8a28b
Jaya Vishwakarma <jayavish423@gmail.com>
2024-11-14 12:47:45 +0530

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,13 @@ export default {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      container: {
+        center : true,
+        padding: "15px" 
       },
+      colors: {
+        accent: "#008000",
+      }
     },
   },
   plugins: [],
